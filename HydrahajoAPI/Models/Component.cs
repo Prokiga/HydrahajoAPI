4ac4c563eb8aa2429baee15bb722909269de2524
Prokai Gábor <prokiga@gmail.com>
2026-06-18 16:32:57 +0200

    Component tábla és DBContext megírva!

---
+++ HydrahajoAPI/Models/Component.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HydrahajoAPI.Models
+{
+    public class Component
+    {
+        [Key]
+        public int Component_Id { get; set; }
+        public string Component_Name { get; set; }
+        public string Component_type { get; set; }
+}
+}
--- HydrahajoAPI/Models/HydrahajoDBContext.cs
+++ HydrahajoAPI/Models/HydrahajoDBContext.cs
@@ -1,7 +1,24 @@
-﻿namespace HydrahajoAPI.Models
-{
-    public class HydrahajoDBContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
-    }
+namespace HydrahajoAPI.Models
+{
+    public class HydrahajoDBContext : DbContext
+    {
+        public HydrahajoDBContext()
+        {
+
+        }
+
+
+        public HydrahajoDBContext(DbContextOptions<HydrahajoDBContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost; Database=Hydrahajo; Uid=root; Pwd=;");
+        }
+}
 }
